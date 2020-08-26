a95970a07b66a36e70a5200de15dba528a0e0a3a
Tejendra <tejasbangalore@protonmail.com>
2020-08-26 22:33:09 +0530

    Completed

--- script.js
+++ script.js
@@ -7,10 +7,56 @@
 //Addition Operation
 plusButton = document.getElementById("addButton");
 plusOperation = function() {
-    alert("Plus Clicked");
+    n1 = document.getElementById("n1").value;
+    n2 = document.getElementById("n2").value;
+
+    sum = parseInt(n1) + parseInt(n2);
+    result = document.getElementById("result");
+    result.innerText = sum;
 }
 
 plusButton.addEventListener("click", plusOperation);
+
+
+subButton = document.getElementById("subButton");
+subOperation = function() {
+    n1 = document.getElementById("n1").value;
+    n2 = document.getElementById("n2").value;
+
+    sub = parseInt(n1) - parseInt(n2);
+    result = document.getElementById("result");
+    result.innerText = sub;
+}
+
+subButton.addEventListener("click", subOperation);
+
+mulButton = document.getElementById("mulButton");
+mulOperation = function() {
+    n1 = document.getElementById("n1").value;
+    n2 = document.getElementById("n2").value;
+
+    mul = parseInt(n1) * parseInt(n2);
+    result = document.getElementById("result");
+    result.innerText = mul;
+}
+
+mulButton.addEventListener("click", mulOperation);
+
+divButton = document.getElementById("divButton");
+divOperation = function() {
+    n1 = document.getElementById("n1").value;
+    n2 = document.getElementById("n2").value;
+
+    div = parseInt(n1) / parseInt(n2);
+    result = document.getElementById("result");
+    result.innerText = div;
+}
+
+divButton.addEventListener("click", divOperation);
+
+
+
+
 
 
 
